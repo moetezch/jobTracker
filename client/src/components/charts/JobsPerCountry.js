4957de28edf46720d51cfb940c186a8814dc7a91
moetez <mo3tez@gmail.com>
2018-08-30 15:51:24 +0100

    added countries flags

--- client/src/components/charts/JobsPerCountry.js
+++ client/src/components/charts/JobsPerCountry.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { GeoChart } from 'react-chartkick'
-
+var CL = require('country-list')();
 
 class JobsPerCountry extends Component {
 
@@ -9,28 +9,39 @@ class JobsPerCountry extends Component {
       (totals, job) => ({ ...totals, [job.country]: (totals[job.country] || 0) + 1 }),
       {}
     )
-
-
     const countries = []
     for (var key in jobsPerCountry) {
       if (jobsPerCountry.hasOwnProperty(key)) {
         countries.push([key, jobsPerCountry[key]])
       }
     }
+    const countriesList=countries.map((country)=>{
+      const flag = `https://www.countryflags.io/${CL.getCode(country[0])}/shiny/32.png`
+      return (
+          <div className="column is-2 has-text-centered" key={country[0]}>
+            <div>
+          <img src={flag} alt="country[0]"/>
+          <p className="is-size-6"> {country[0]} : {country[1]}</p>
+          </div>
+          </div>
     
+      )
+    })    
     return (
+      <div className="container">
       <GeoChart data={countries} height="500px" />
+      <div className="columns is-multiline">
+      {countriesList}
+      </div>
+      </div>
     )
   }
   render() {
     return (
       
-      <div>
+      <div >
         <h2 className="is-size-3">Jobs/Country</h2>
-        <div>
           {this.renderJobsPerCountry()}
-        </div>
-
       </div>
     )
   }
